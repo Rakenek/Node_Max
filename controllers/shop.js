3a204e3ff0958836be4ab6516162d54d15866729
Rakenek <kamilotawski@gmail.com>
2022-08-06 20:02:51 +0200

    200

--- controllers/shop.js
+++ controllers/shop.js
@@ -43,17 +43,13 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .getCart()
-    .then((cart) => {
-      return cart
-        .getProducts()
-        .then((products) => {
-          res.render("shop/cart", {
-            path: "/cart",
-            pageTitle: "Your Cart",
-            products: products,
-          });
-        })
-        .catch((err) => console.log(err));
+    .then((products) => {
+      console.log(products);
+      res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart",
+        products: products,
+      });
     })
     .catch((err) => console.log(err));
 };
@@ -66,6 +62,7 @@ exports.postCart = (req, res, next) => {
     })
     .then((result) => {
       console.log(result);
+      res.redirect("/cart");
     });
   // let fetchedCart;
   // let newQuantity = 1;
