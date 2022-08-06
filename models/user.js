a18f1098d55cfd19d375d1f245e1439c5515051a
Rakenek <kamilotawski@gmail.com>
2022-08-06 20:49:06 +0200

    203

--- models/user.js
+++ models/user.js
@@ -77,15 +77,28 @@ class User {
 
   addOrder() {
     const db = getDb();
-    return db
-      .collection("orders")
-      .insertOne(this.cart)
+    return this.getCart()
+      .then((products) => {
+        const order = {
+          items: products,
+          user: {
+            _id: new ObjectId(this._id),
+            name: this.name,
+          },
+        };
+        return db.collection("orders").insertOne(order);
+      })
       .then((result) => {
         this.cart = { items: [] };
         return db
           .collection("users")
           .updateOne({ _id: this._id }, { $set: { cart: { items: [] } } });
       });
+  }
+
+  getOrders() {
+    const db = getDb();
+    // return db.collection('orders').
   }
 
   static findById(userId) {
