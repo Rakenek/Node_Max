c8046dbb8fcad6b30d9df0ec4a2ad6a376bf130f
Rakenek <kamilotawski@gmail.com>
2022-08-11 20:10:56 +0200

    242

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const mongoose = require("mongoose");
+const session = require("express-session");
 const MONGO_PASS = require("./SECRET");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -10,7 +12,13 @@ const authRoutes = require("./routes/auth");
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
+const MONGODB_URI = `mongodb+srv://raken:${MONGO_PASS}@cluster0.t7o7cnp.mongodb.net/shop?retryWrites=true&w=majority`;
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -18,6 +26,14 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
 
 app.use((req, res, next) => {
   const id = "62f2b4072068ea1ce259de4b";
@@ -37,9 +53,7 @@ app.use(authRoutes);
 app.use(errorController.notFoundProducts);
 
 mongoose
-  .connect(
-    `mongodb+srv://raken:${MONGO_PASS}@cluster0.t7o7cnp.mongodb.net/shop?retryWrites=true&w=majority`
-  )
+  .connect(MONGODB_URI)
   .then((result) => {
     User.findOne().then((user) => {
       if (!user) {
