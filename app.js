0b9828d49f00ab6a7a882bfebe5063764a8a4058
Rakenek <kamilotawski@gmail.com>
2022-08-13 21:54:43 +0200

    269

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const session = require("express-session");
 const MONGO_PASS = require("./SECRET");
 const MongoDBStore = require("connect-mongodb-session")(session);
 const csrf = require("csurf");
+const flash = require("connect-flash");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -37,6 +38,7 @@ app.use(
   })
 );
 app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
   if (!req.session.user) {
