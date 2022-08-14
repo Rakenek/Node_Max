438eee503c03e006c06761f0d54a1beb2e5acc4e
Rakenek <kamilotawski@gmail.com>
2022-08-14 09:42:11 +0200

    287

--- controllers/admin.js
+++ controllers/admin.js
@@ -60,15 +60,17 @@ exports.postEditProduct = (req, res, next) => {
 
   Product.findById(prodId)
     .then((product) => {
+      if (product.userId.toString() !== req.user._id.toString()) {
+        return res.redirect("/");
+      }
       product.title = updatedTitle;
       product.price = updatedPrice;
       product.imageUrl = updatedImageUrl;
       product.description = updatedDesc;
-      return product.save();
-    })
-    .then((result) => {
-      console.log("updated product");
-      res.redirect("/admin/products");
+      return product.save().then((result) => {
+        console.log("updated product");
+        res.redirect("/admin/products");
+      });
     })
     .catch((e) => {
       console.log(error);
@@ -92,7 +94,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
+  Product.find({ userId: req.user._id })
     .then((products) => {
       res.render("admin/products", {
         prods: products,
@@ -107,7 +109,7 @@ exports.getProducts = (req, res, next) => {
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
 
-  Product.findByIdAndRemove(prodId)
+  Product.deleteOne({ _id: prodId, userId: req.user._id })
     .then((result) => {
       res.redirect("/admin/products");
     })
